e44fba8c7a72aaa3856551c388546e16883f7ba6
Vivekv1999 <vivekvekariya1999@gmail.com>
2022-11-17 17:10:50 +0530

    REST api --all

---
+++ src/db/conn.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/olympics").then(() => {
+    console.log('connection is successfulluy to mongobd compas')
+}).catch((e) => {
+    console.log('no connection')
+})
